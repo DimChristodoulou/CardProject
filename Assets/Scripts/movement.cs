3101381d7338b31d1d898713475221ba34df5291
DimChristodoulou <dchristodoulou.di@gmail.com>
2017-12-04 14:36:03 +0200

    Clickable working for movement = 2
    Fixed corner cases

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnMouseDown()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
