4f577a1d1ea151e1b8d55562b9e86fa83e2a5537
dgounaris <dgoungr@hotmail.com>
2017-12-04 13:53:44 +0200

    Added gamestate and player

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	public Player player;
+	private Player[] players;
+	private int numOfPlayers;
+	private int activePlayerIndex;
+	private float moveTime;
+	private float currentMoveTime;
+
+	// Use this for initialization
+	void Start () {
+		setPlayers (2);
+		moveTime = 10.0f;
+		activePlayerIndex = -1;
+		nextPlayerTurn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentMoveTime -= Time.deltaTime;
+		if (currentMoveTime <= 0) { //end of turn
+			nextPlayerTurn();
+		}
+		//to get seconds in int value use:
+		//Mathf.Ceil(currentMoveTime);
+		//Debug.Log("Playing " + players[activePlayerIndex].getName() + " time " + Mathf.Ceil(currentMoveTime));
+	}
+
+	public void setPlayers(int playersNum) { //will probably change to array of Player later on
+		numOfPlayers = playersNum;
+		players = new Player[numOfPlayers];
+		int i = 0;
+		for (; i < numOfPlayers; i++) {
+			players [i] = Instantiate (player);
+			players [i].setName ("Player" + (i + 1));
+		}
+	}
+
+	void nextPlayerTurn() {
+		if (activePlayerIndex == -1) {
+			activePlayerIndex = 0;
+			players [activePlayerIndex].switchPlayingState ();
+		}
+		else {
+			players [activePlayerIndex].switchPlayingState ();
+			activePlayerIndex = (activePlayerIndex + 1) % numOfPlayers;
+			players [activePlayerIndex].switchPlayingState ();
+		}
+		currentMoveTime = moveTime; //reset time
+		//add Player functions to start turn properly here, using players[activePlayerIndex]
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private string name;
+	private GameObject hero; //hero is a seperate unit due to its win condition checks and the fact that he is instantiated from beginning
+	private List<GameObject> handCards;
+	private List<GameObject> boardMinions;
+	private List<GameObject> graveyard;
+	private List<GameObject> deck;
+	private bool isPlaying;
+
+	void Start() {
+		isPlaying = false;
+	}
+
+	void Update() {
+
+	}
+
+	public bool getPlayingState() {
+		return this.isPlaying;
+	}
+
+	public void switchPlayingState() {
+		this.isPlaying = !this.isPlaying;
+	}
+
+	public string getName() {
+		return this.name;
+	}
+
+	public void setName(string mName) {
+		name = mName;
+	}
+
+}
