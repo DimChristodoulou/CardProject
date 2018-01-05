910158f4a1025f948ad8c5a79584acd65aef711c
AntonisKl <denti20082008@hotmail.com>
2018-01-05 02:36:10 +0200

    Fixed error with UI elements of card
    Card is displayed properly and dynamically

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -17,6 +17,9 @@ public class GameState : MonoBehaviour {
 		moveTime = 10.0f;
         //
         GameObject cardsample = Instantiate<GameObject>( (GameObject)Resources.Load("CardDisplaySample") );
+        cardsample.transform.SetParent(GameObject.Find("Main UI").transform, false);
+
+        cardsample.GetComponent<Renderer>().enabled = true;
         //cardsample.transform.SetParent(GameObject.Find("Canvas").transform, false);
         //
 		activePlayerIndex = -1;
